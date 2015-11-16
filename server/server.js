b3b36eb48d2069b87c9f1db729491ab6533ba09b
Martin Nielsen <knownwater@gmail.com>
2015-11-16 13:59:39 +0100

    basic server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.use(require('./router.js'));
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Listening at http://%s:%s', host, port);
+});
