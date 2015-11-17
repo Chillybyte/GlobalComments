0022c6b241548aaabd2b773e59c5b243c9dcbcd0
Emil Timander <crabwolf@gmail.com>
2015-11-17 21:57:47 +0100

    Deleted config from cache

--- server/config.js
+++
@@ -1,6 +0,0 @@
-process.env.APP_MONGOOSE_DRIVER = 'mongodb://';
-process.env.APP_MONGOOSE_USER = '';
-process.env.APP_MONGOOSE_PASSWORD = '';
-process.env.APP_MONGOOSE_HOST = 'localhost';
-process.env.APP_MONGOOSE_PORT = '27017';
-process.env.APP_MONGOOSE_DB = '/global_comments';
