e3dc08a6c1f9bd6d2b7907cb3c44f9ea49907ba3
Emil Timander <crabwolf@gmail.com>
2015-11-19 15:15:41 +0100

    WIP for at kunne oprette en person

--- web/app/components/comp-topbar/controller.js
+++ web/app/components/comp-topbar/controller.js
@@ -3,10 +3,36 @@ APP.directive('compTopbar', [function() {
 
     return {
         replace: true,
-        restrict: "E",
-        templateUrl: "components/comp-topbar/template.html",
-        controller: ['$scope', function($scope) {
+        restrict: 'E',
+        templateUrl: 'components/comp-topbar/template.html',
+        controller: ['$scope', '_http', '_notifications', function($scope, _http, _notifications) {
+            $scope.username = '';
+            $scope.first_name = '';
+            $scope.last_name = '';
+            $scope.email = '';
+            $scope.password = '';
+            $scope.repeat_password = '';
 
+            $scope.sign_up = function() {
+                if (!$scope.username) {
+                    _notifications.ERROR("Username is empty")
+                } else if (!$scope.email) {
+                    _notifications.ERROR("email is empty")
+                } else if (!$scope.password) {
+                    _notifications.ERROR("password is empty")
+                } else if (!$scope.repeat_password) {
+                    _notifications.ERROR("Password repeated is empty")
+                } else {
+                    _http.post("/api/open/session", {
+                        username: $scope.username,
+                        first_name: $scope.first_name,
+                        last_name: $scope.last_name,
+                        email: $scope.email,
+                        password: $scope.password,
+                        repeat_password: $scope.repeat_password
+                    })
+                }
+            }
         }]
     };
 
