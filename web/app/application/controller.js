484d3759c4ae3b8652437e9093fb08f1198c994b
Rane Eriksen <re@shipfeed.dk>
2015-11-23 16:14:08 +0100

    Removed test _notification from controller

--- web/app/application/controller.js
+++ web/app/application/controller.js
@@ -1,6 +1,3 @@
 var APP = angular.module("APP");
 APP.controller("applicationController", ["$scope", "_notifications", function($scope, _notifications) {
-    _notifications.ERROR("test");
-    _notifications.SUCCESS("test");
-    _notifications.INFO("test");
 }]);
