8bfefa46340770d9244f2f2d1641eebf5cc11828
Rane Eriksen <re@shipfeed.dk>
2015-11-23 16:14:50 +0100

    Added first route to index in router.js and made controller and fake template for the site

---
+++ web/app/application/index/controller.js
@@ -0,0 +1,2 @@
+var APP = angular.module("APP");
+APP.controller("indexController", ["$scope", function($scope) {}]);
---
+++ web/app/router.js
@@ -0,0 +1,35 @@
+var APP = angular.module("APP");
+
+APP.config(["$locationProvider", "$stateProvider", "$urlRouterProvider", function($locationProvider, $stateProvider, $urlRouterProvider) {
+    var _initialized = false;
+    /**
+     * Happens whenever the user navigates
+     */
+    $urlRouterProvider
+        .when("/{path:.*}", ["_user", "$state", function(_user) {
+            if (!_initialized) {
+                _initialized = true;
+                _user.authenticate();
+            }
+            return false; //Tell router that is has not yet resolved the path - Continue to $stateProvider
+        }]);
+
+    $stateProvider
+        .state("index", {
+            url: "/",
+            controller: "indexController",
+            templateUrl: "application/index/template.html",
+            params: {
+                contact: null
+            }
+        });
+
+
+
+    $urlRouterProvider
+        .otherwise(function() {
+            alert("DOH! - This site does not exist!");
+        });
+
+    $locationProvider.html5Mode(true); //Removes the HASHTAG
+}]);
