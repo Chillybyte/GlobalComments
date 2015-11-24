564f26b24e462d9f2ae0dfd52c8cff6ef3363abf
Rane Eriksen <re@shipfeed.dk>
2015-11-24 16:49:16 +0100

    Added support for collecting git commits to about page

--- web/app/application/about/controller.js
+++ web/app/application/about/controller.js
@@ -1,8 +1,16 @@
 var APP = angular.module("APP");
-APP.directive("about", [function() {
-    return {
-        replace: true,
-        restrict: "E",
-        templateUrl: "application/about/template.html"
-    };
+APP.controller("aboutController", ["$scope", "_http", function($scope, _http) {
+    $scope.git_log = [];
+
+    _http.get("//api.github.com/repos/Chillybyte/GlobalComments/commits", null, {
+            silent: true
+        })
+        .then(function(result) {
+            $scope.git_log = result.data;
+            console.log(result);
+        })
+        .catch(function(err) {
+            $scope.git_log = [];
+            console.trace(err);
+        });
 }]);
