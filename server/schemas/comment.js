2c278d27bd73d77bc0f657ace7e035e8dad126e1
Martin Nielsen <knownwater@gmail.com>
2015-11-24 12:17:14 +0100

    comment schema added

---
+++ server/schemas/comment.js
@@ -0,0 +1,39 @@
+var mongoose = require("mongoose"),
+    Schema = mongoose.Schema,
+
+var commentSchema = new Schema({
+
+        reference: {
+            type: String,
+            required: true,
+            unique: false,
+            index: true
+        },
+
+        user_id: {
+            type: String,
+            required: true,
+            unique: false,
+            index: true
+        },
+        
+        content: {
+            type: String,
+            required: true,
+            unique: false,
+            index: true
+        },
+
+        date_time: {
+            creation_time: Date.now();
+            required: true,
+            unique: false,
+            }
+        },
+
+    }, {
+        strict: true //Only data relevant to this schema is saved
+    })
+
+
+module.exports = mongoose.model("comment", commentSchema);
