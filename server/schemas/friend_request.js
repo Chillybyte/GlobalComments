f28cf2694e4be0407d230bd15e2b681265da0316
Emil Timander <crabwolf@gmail.com>
2015-11-26 11:57:20 +0100

    Friend list, and request schemas added.

---
+++ server/schemas/friend_request.js
@@ -0,0 +1,58 @@
+var mongoose = require("mongoose"),
+    Schema = mongoose.Schema;
+
+
+var friend_request = new Schema({
+
+    user: { //Person who requested a user to be friends
+        type: Schema.Types.ObjectId,
+        required: true,
+        unique: false,
+        index: true
+    },
+
+    user_requested: { //Person who is requested to be friends
+        type: Schema.Types.ObjectId,
+        required: true,
+        unique: false,
+        index: true
+    },
+
+    answer: { //Yes i want to be friends = true, no i dont want to be friend false.
+        type: boolean,
+        required: true,
+        unique: false,
+        index: false,
+        default: false
+    },
+
+    answered: { //Have the requested user answered yes or no, or is it still unanswered
+        type: boolean,
+        required: true,
+        unique: false,
+        index: false,
+        default: false
+    },
+
+    created_at: { //Date of created
+        type: Date,
+        required: true,
+        unique: false,
+        default: Date.now
+    },
+
+    updated_at: { //Date updated at
+        type: Date,
+        required: true,
+        unique: false,
+        default: Date.now
+    }
+
+}, {
+    strict: true //Only data relevant to this schema is saved
+}).pre("save", function(next) {
+    var _now = new Date();
+    //On every update, this variable is update to current date
+    this.updated_at = _now;
+    next();
+});
