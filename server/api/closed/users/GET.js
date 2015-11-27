2867e581699fc174a0f70e98cb6cf3bbb2e5d8c3
Rane Eriksen <re@shipfeed.dk>
2015-11-27 22:43:26 +0100

    Added api to collect users to closed api

---
+++ server/api/closed/users/GET.js
@@ -0,0 +1,26 @@
+var SCHEMA_USER = require(process.env.APP_SCHEMA_USER);
+module.exports = function(_request, _response) {
+    SCHEMA_USER.findOne(_request.params.user_id)
+        .then(function(user) {
+            if (user) {
+                _response
+                    ._R
+                    ._DATA("user", user.toJSON())
+                    ._SEND();
+            } else {
+                _response
+                    ._R
+                    ._ERROR("Friend does not exist")
+                    ._STATUS(400)
+                    ._SEND();
+            }
+        })
+        .catch(function(err) {
+            _response
+                ._R
+                ._ERROR("Failed to find friend")
+                ._STATUS(500)
+                ._SEND();
+            console.trace(err);
+        });
+};
