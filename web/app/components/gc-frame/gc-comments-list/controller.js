967e5a4b279f79595b053fc7e16f751f5499e38a
Martin Nielsen <knownwater@gmail.com>
2015-11-29 21:51:54 +0100

    functional comment track fetching with dummy data

--- web/app/components/gc-frame/gc-comments-list/controller.js
+++ web/app/components/gc-frame/gc-comments-list/controller.js
@@ -5,11 +5,35 @@ APP.directive("gcCommentsList", [function() {
         restrict: "E",
         show_left_pane: "=",
         templateUrl: "components/gc-frame/gc-comments-list/template.html",
-        controller: ["$scope", function($scope) {
+        controller: ["$scope", "_user", "_comments", function($scope, _user, _comments) {
 
-            $scope.set_left = function(value) {
+            //$scope.commentThreadDetails = true;
+            //$scope.show_left_pane = false;
+
+
+            // $scope.comment = _user.comments;
+            // var user_tests = ["en kommentartråd", "en anden kommentartråd", "en mere kommentartråd"];
+            var user_tests = [{
+                    reference: "en kommentartråd",
+                    comments: "Indhold fra den første kommentar spor"
+                }, {
+                    reference: "en anden kommentartråd",
+                    comments: "Indhold fra den anden kommentar spor"
+                }, {
+                    reference: "en mere kommentartråd",
+                    comments: "Indhold fra et kommentar spor mere"
+                }
+
+            ];
+
+            $scope.data = user_tests;
+            $scope.set_left_pane = function(value, sectionIndex) {
+                console.log(sectionIndex);
+                console.log($scope.show_left_pane);
+                $scope.comments = user_tests[sectionIndex];
                 $scope.show_left_pane = value;
             };
+
         }]
     };
 }]);
