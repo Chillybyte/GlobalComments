86c72a8287f87158b42c862331c3570ed8bb1961
Rane Eriksen <re@shipfeed.dk>
2015-11-30 21:43:43 +0100

    Added base service for chat

--- web/app/services/_chat.js
+++ web/app/services/_chat.js
@@ -3,27 +3,35 @@ APP.service("_chat", ["_http", function(_http) {
 
     this.threads = {};
 
-    this.comments = function(reference) {
-        var _this = this;
-        if (_this.threads[reference])
-            return _this.threads[reference];
-        else
-            _this.threads[reference] = [];
+    this.new_conversation = function(user_id) {
 
-        _http.get("/api/open/thread_chat/" + reference)
-            .then(function(result) {
-                angular.copy(result.data.thread, _this.threads[reference]);
-            });
-        return _this.threads[reference];
     };
 
-    this.new_comment = function(reference, message) {
+    this.add_person_to_conversation = function(thread_id, user_id) {
+
+    };
+
+    this.get_messages = function(thread_id) {
         var _this = this;
-        return _http.post("/api/closed/thread_chat/" + reference, {
+        if (_this.threads[thread_id])
+            return _this.threads[thread_id];
+        else
+            _this.threads[thread_id] = [];
+
+        _http.get("/api/open/thread_chat/" + thread_id)
+            .then(function(result) {
+                angular.copy(result.data.thread, _this.threads[thread_id]);
+            });
+        return _this.threads[thread_id];
+    };
+
+    this.new_message = function(thread_id, message) {
+        var _this = this;
+        return _http.post("/api/closed/thread_chat/" + thread_id, {
                 message: message
             })
             .then(function(result) {
-                _this.threads[reference].messages.push(result.data.message);
+                _this.threads[thread_id].messages.push(result.data.message);
             });
     };
 
