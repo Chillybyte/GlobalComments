52d14e43db27733dbaa18902128ac9d9ceea1c56
Rane Eriksen <re@shipfeed.dk>
2015-12-03 15:47:01 +0100

    Made use of the new function 'get_id_list_of_fiends' defined in 'friendSchema'

--- server/socket.js
+++ server/socket.js
@@ -1,17 +1,20 @@
+require('./config.js'); //SECRET HUSH HUSH FILE DO NOT SHARE
+require('./settings.js');
 var express = require('express'),
     http = require('http'),
-    io = require('socket.io'),
-    passportSocketIo = require('passport.socketio');
+    IO = require('socket.io'),
+    passportSocketIo = require('passport.socketio'),
+    SCHEMA_FRIEND = require(process.env.APP_SCHEMA_FRIEND);
 
 var exports = module.exports = function(app, sessionStore, cookieParser) {
 
     //Creates the server object for the socket to listen on.
-    var socket = io(app);
+    var io = IO(app);
 
     /**
      *  Authorize users when they connect via sockets
      */
-    socket.use(passportSocketIo.authorize({
+    io.use(passportSocketIo.authorize({
         cookieParser: cookieParser,
         key: process.env.APP_SERVER_KEY,
         secret: process.env.APP_SERVER_SECRET,
@@ -25,36 +28,66 @@ var exports = module.exports = function(app, sessionStore, cookieParser) {
      *  Use this to set initial rooms for the socket
      */
     function on_auth_success(socket, accept) {
-//        console.log(socket.user);
-
+        console.log("Auth success");
         /**
          *  Accepts the connection
          *  A message may be sent to client via accept
          */
         accept();
-    }
+    };
 
     /**
      *  Callback function i user was not signed in successfully
      *  Set back a message for the client
      */
     function on_auth_fail(socket, message, error, accept) {
+        console.trace("Auth failed");
         /**
          *  Send back a message for the client if needed
          */
         if (error)
             accept(new Error(message));
-    }
+    };
 
     /**
      *  All event listeners goes in this section
      */
-    socket.on('connection', function(socket) {
+    io.on('connection', function(socket) {
         var _user = socket.conn.request.user;
         console.log('Connection made by ' + _user.username);
+        socket.join(_user._id, function(err) {
+            console.log(_user.username + " joined room " + _user._id);
+            if (!err) {
+                //Let's go tell all friends that _user joined the chat
+                SCHEMA_FRIEND.get_id_list_of_fiends(_user._id)
+                    .then(function(friend_ids) {
+                        friend_ids.forEach(function(friend_id) {
+                            socket.to(friend_id);
+                        });
+                        socket.emit("friend_online_status", {
+                            friend: _user._id,
+                            online: true
+                        });
+
+                    });
+            } else
+                console.trace(err);
+        });
 
         socket.on('disconnect', function() {
-            console.log(_user.username + ' disconnected');
+            var room = io.sockets.adapter.rooms[_user._id];
+            if (!room) {
+                SCHEMA_FRIEND.get_id_list_of_fiends(_user._id)
+                    .then(function(friend_ids) {
+                        friend_ids.forEach(function(friend_id) {
+                            socket.to(friend_id);
+                        });
+                        socket.emit("friend_online_status", {
+                            friend: _user._id,
+                            online: false
+                        });
+                    });
+            }
         });
     });
 };
