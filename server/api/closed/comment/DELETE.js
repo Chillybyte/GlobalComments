7faf2adb357ba3aa9fb7ad57c5e64659dd0c9873
Martin Nielsen <fremadbagud@gmail.com>
2015-12-05 00:45:17 +0100

    ruff version, but kind of working delete in mongodb

--- server/api/closed/comment/DELETE.js
+++ server/api/closed/comment/DELETE.js
@@ -3,16 +3,11 @@ var APP_SCHEMA_USER = require(process.env.APP_SCHEMA_USER),
     SCHEMA_MESSAGE = require(process.env.APP_SCHEMA_MESSAGE);
 
 module.exports = function(_request, _response) {
-    console.log(_request.params.delete + '  inside DELETE.js on the server side');
 
-    	SCHEMA_THREAD_COMMENT.findOne({
-    		_id: _request.params.delete
+    	SCHEMA_THREAD_COMMENT.remove({
+    		'messages._id' : _request.params.delete
     	})
     	.then(function(message) {
     		console.log(message);
-    		
     	});
-
-
-
 };
